03fa8f83f729e964261241e7d1a6249b51ec7359
Hanifff <mohamad.h@hotmail.no>
2021-11-24 14:43:55 +0000

    renconfig network - update readme

--- caliper-workspace/bench_config.py
+++ caliper-workspace/bench_config.py
@@ -1,13 +1,29 @@
 import yaml
 import os
+import glob
 
 
-def main():
+def reconfig_network(file_path: str = './networks/networkConfig.yaml') -> None:
+    with open(file_path) as f:
+        net_config = yaml.safe_load(f)
+
+    sk_path = "../../fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/*sk"
+
+    sk_file = []
+    for file in glob.glob(sk_path):
+        sk_file.append(file)
+
+    net_config['organizations'][0]['identities']['certificates'][0]['clientPrivateKey']['path'] = sk_file[0] if "sk" in sk_file[0] else ""
+
+    with open(file_path, 'w') as f:
+        yaml.dump(net_config, f)
+
+
+def reconfig_benchmark(file_path: str = './benchmarks/myAssetBenchmark.yaml') -> None:
     """
-    This function dynamically resets configurations of benchmark module for the caliper-workspace.
+        This function dynamically resets configurations of benchmark module for the caliper-workspace.
     """
-
-    with open('./benchmarks/myAssetBenchmark.yaml') as f:
+    with open(file_path) as f:
         bench_config = yaml.safe_load(f)
 
     bench_config['test']['workers']['number'] = int(
@@ -16,9 +32,5 @@ def main():
         bench_config['test']['rounds'][round]['rateControl']['opts']['tps'] = int(
             os.environ['TPS_Caliper'])
 
-    with open('./benchmarks/myAssetBenchmark.yaml', 'w') as f:
+    with open(file_path, 'w') as f:
         yaml.dump(bench_config, f)
-
-
-if __name__ == "__main__":
-    main()
--- caliper-workspace/run.sh
+++ caliper-workspace/run.sh
@@ -6,9 +6,13 @@
 set -e
 starttime=$(date +%s)
 
+# Benchmark different transaction data sizes, nr of clients, and tps with default config - 2org- 2peers. 1 orderer, batchto: 2s, pblock size: 512 KB.
 declare -a sizes=('data1024B' 'data2kB' 'data3kB' 'data4kB' 'data5kB')
 declare -a clients=('2' '10' '20' '50')
 declare -a tps=('25' '50' '100' '200')
+
+# reconfigure the network config file with new secret key
+python3 -c "import bench_config; bench_config.reconfig_network()"
 
 
 for i in "${sizes[@]}"; do
@@ -19,7 +23,7 @@ for i in "${sizes[@]}"; do
             export TPS_Caliper="$h"
             echo "Running tests for collection: $i, nr of clients: $j, and tps: $h"
             # rewrite the benchmark configuration 
-            python3 bench_config.py
+            python3 -c  "import bench_config; bench_config.reconfig_benchmark()"
             # sleep for 10 second for configuration to be applied
             sleep 10
             # run the caliper cli
@@ -33,6 +37,33 @@ for i in "${sizes[@]}"; do
 done
 
 
+# benchmark by adding an org
+
+dataSize='data1024b'
+nrOfclients='10'
+declare -a tps=('25' '50' '100' '200')
+export COLLECTION_TYPE=dataSize
+export NR_OF_CLINETS=nrOfclients
+
+for h in "${tps[@]}"; do
+    export TPS_Caliper="$h"
+    echo "Running tests for tps: $h"
+    # rewrite the benchmark configuration 
+    python3 -c  "import bench_config; bench_config.reconfig_benchmark()"
+    # sleep for 10 second for configuration to be applied
+    sleep 5
+    # run the caliper cli
+    npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/networkConfig.yaml \
+    --caliper-benchconfig benchmarks/myAssetBenchmark.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled \
+    --caliper-report-path results/report_3org_1024kb_10cli_"$h"_tps.html
+    # sleep for 5 sec before restarting
+    sleep 5
+done
+
+
+# benchmark by changing block size
+# Benchmark different blocksize x,x,x with the same datasize and nrofclients, vary tps
+
 cat <<EOF
 
 Total setup execution time : $(($(date +%s) - starttime)) secs ...
@@ -43,10 +74,8 @@ Then, to see the results:
 EOF
 
 
-# benchmark by chaning batch timeout
 
-# benchmark by changing block size
 
-# benchmark by adding an org
+
 
 # benchmark by adding peers?
